63fdea83ceb1fddd2082e9187a92eb3985a7ee40
tamuna-momo <tamuna-momo@users.noreply.github.com>
2021-10-27 00:01:34 +0400

    task #5

--- app.js
+++ app.js
@@ -1,50 +1,60 @@
-// 1.
-const currentDay=['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thusday', 'Friday', 'Satuday',]
-switch(currentDay) {
-    case 0:
-        console.log('Sunday');
-        break;
-    case 1:
-        console.log('Monday')  ;
-        break;
-    case 2:
-        console.log('Tuesday') ;
-        break;
-    case 3:
-        console.log('Wednesday');
-        break;
-    case 4:
-        console.log('Thusday');
-        break;
-    case 5:
-        console.log('Friday');
-        break;
-    case 6:
-        console.log('Saturday');
-        break;                 
-}
-console.log(currentDay[0]);
-console.log(currentDay[1]);
-console.log(currentDay[2]);
-console.log(currentDay[3]);
-console.log(currentDay[4]);
-console.log(currentDay[5]);
-console.log(currentDay[6]);
+// // 1.
+// const currentDay=['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thusday', 'Friday', 'Satuday',]
+// switch(currentDay) {
+//     case 0:
+//         console.log('Sunday');
+//         break;
+//     case 1:
+//         console.log('Monday')  ;
+//         break;
+//     case 2:
+//         console.log('Tuesday') ;
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thusday');
+//         break;
+//     case 5:
+//         console.log('Friday');
+//         break;
+//     case 6:
+//         console.log('Saturday');
+//         break;                 
+// }
+// console.log(currentDay[0]);
+// console.log(currentDay[1]);
+// console.log(currentDay[2]);
+// console.log(currentDay[3]);
+// console.log(currentDay[4]);
+// console.log(currentDay[5]);
+// console.log(currentDay[6]);
 
-// 2.
-for (i=0; i<100; i++){
-    console.log(i);
-}
+// // 2.
+// for (i=0; i<100; i++){
+//     console.log(i);
+// }
  
-// 3.
-i=0;
-while (i<50) {
-    console.log(i);
-    i++;
-}
+// // 3.
+// i=0;
+// while (i<50) {
+//     console.log(i);
+//     i++;
+// }
 
-// 4.
-do {
-     i++;
-     console.log(i);
-} while (i<150);
+// // 4.
+// do {
+//      i++;
+//      console.log(i);
+// } while (i<150);
+
+// 5. 
+// numbers=[];
+// for( let i=0; i<10000; i++){
+//     console.log(i * i);   
+// }
+// numbers.push(i * i);
+
+// 6.
+
