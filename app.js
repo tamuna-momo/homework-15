de2d578d1b2be623b0c0d8a4c767c799fe59d5a6
tamuna-momo <tamuna-momo@users.noreply.github.com>
2021-10-26 10:59:38 +0400

    homework_task #1

---
+++ app.js
@@ -0,0 +1,32 @@
+// 1.
+const currentDay=['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thusday', 'Friday', 'Satuday',]
+switch(currentDay) {
+    case 0:
+        console.log('Sunday');
+        break;
+    case 1:
+        console.log('Monday')  ;
+        break;
+    case 2:
+        console.log('Tuesday') ;
+        break;
+    case 3:
+        console.log('Wednesday');
+        break;
+    case 4:
+        console.log('Thusday');
+        break;
+    case 5:
+        console.log('Friday');
+        break;
+    case 6:
+        console.log('Saturday');
+        break;                 
+}
+console.log(currentDay[0]);
+console.log(currentDay[1]);
+console.log(currentDay[2]);
+console.log(currentDay[3]);
+console.log(currentDay[4]);
+console.log(currentDay[5]);
+console.log(currentDay[6]);
